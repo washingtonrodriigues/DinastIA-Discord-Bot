b0a849d5f60ead98ff0f5ba7a71a5d3e64858500
Washington Rodrigues <washrodriigues@gmail.com>
2025-03-03 16:51:05 -0300

    feat: delete the private channel when the user leaves from the server

---
+++ memberLeaveHandler.js
@@ -0,0 +1,19 @@
+export async function handleMemberLeave(member) {
+    const guild = member.guild;
+  
+    const channel = guild.channels.cache.find(
+      (c) => c.name === `${member.user.username}` && c.parent?.name === 'ONBOARDING'
+    );
+  
+    if (channel) {
+      try {
+        await channel.delete();
+        console.log(`🗑️ Canal ${channel.name} excluído porque ${member.user.tag} saiu do servidor.`);
+      } catch (error) {
+        console.error(`❌ Erro ao excluir o canal: ${error}`);
+      }
+    } else {
+      console.log(`ℹ️ Nenhum canal encontrado para ${member.user.tag}.`);
+    }
+  }
+  
