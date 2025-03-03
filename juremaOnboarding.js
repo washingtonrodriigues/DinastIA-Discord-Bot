ad1703a4e9186d59aa9c4d16017acaee4ce8938c
Washington Rodrigues <washrodriigues@gmail.com>
2025-03-03 16:52:32 -0300

    feat: add jurema request

---
+++ juremaOnboarding.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export default async function juremaOnboarding(message, webhookUrl) {
+
+    const roles = message.member.roles.cache.map(role => role.id);
+
+  try {
+    const response = await axios.post(webhookUrl, {
+        username: message.author.username,
+        user: message.member.displayName,
+        roles: roles,
+        question: message.content,
+        channelId: message.channel.id,
+    });
+
+    if (response.data.output) {
+      message.reply({
+        content: response.data.output,
+        allowedMentions: { parse: [] },
+        flags: 1 << 2,
+      });
+    }
+  } catch (error) {
+    console.error('Erro ao acessar o n8n:', error);
+  }
+}
