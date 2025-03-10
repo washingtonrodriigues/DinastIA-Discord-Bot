ff6a59b33d2226c22cf77844be58f7f0a04564d3
Washington Rodrigues <washrodriigues@gmail.com>
2025-03-10 14:38:37 -0300

    feat: get role name

--- juremaOnboarding.js
+++ juremaOnboarding.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
 export default async function juremaOnboarding(message, webhookUrl) {
-
-    const roles = message.member.roles.cache.map(role => role.id);
+  const roles = message.member.roles.cache.map((role) => role.name);
 
   try {
     const response = await axios.post(webhookUrl, {
-        username: message.author.username,
-        user: message.member.displayName,
-        roles: roles,
-        question: message.content,
-        channelId: message.channel.id,
+      username: message.author.username,
+      user: message.member.displayName,
+      roles: roles,
+      question: message.content,
+      channelId: message.channel.id,
     });
 
     if (response.data.output) {
